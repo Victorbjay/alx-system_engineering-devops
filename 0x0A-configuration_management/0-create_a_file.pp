803bef4e7aead4e20445e5cab80a8ed1f2955bec
Victor Okpe <okpevictor84@gmail.com>
2024-01-21 22:23:18 -0800

    pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# A puppet script that creates a file in /tmp
+
+file { 'school':
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
+}
